57b9310749e07a33680b051f26932584861f4862
NoWayOut5 <veselkovmv@gmail.com>
2021-03-21 16:28:26 +0500

    Изменение урла бэка

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://taxicrm.herokuapp.com',
+  baseURL: 'https://taxicrmback-prod.herokuapp.com',
   timeout: 30000,
 });
 
